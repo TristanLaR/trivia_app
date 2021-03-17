668989703c14d92ee5fa408283ef40f365f0f0a4
Tristan <tristan@larusic.com>
2021-03-17 17:09:53 +0000

    Implement QuizResults Widget

--- lib/main.dart
+++ lib/main.dart
@@ -109,3 +109,81 @@ class QuizScreen extends HookWidget {
           );
   }
 }
+
+//TODO QuizError
+class QuizError extends StatelessWidget {
+  final String message;
+
+  const QuizError({
+    Key key,
+    @required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// TODO CustomButton
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class QuizResults extends StatelessWidget {
+  final QuizState state;
+  final List<Question> questions;
+
+  const QuizResults({
+    Key key,
+    @required this.state,
+    @required this.questions,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          '${state.correct.length} / ${questions.length}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 60.0,
+            fontWeight: FontWeight.w600,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const Text(
+          'CORRECT',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 48.0,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 40.0),
+        CustomButton(
+          title: 'New Quiz',
+          onTap: () {
+            context.refresh(quizRepositoryProvider);
+            context.read(quizControllerProvider);
+          },
+        ),
+      ],
+    );
+  }
+}
+
+//TODO QuizQuestions
+class QuizQuestions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
