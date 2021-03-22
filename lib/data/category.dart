ebae881888252dfe2280b2f978992454e0f0e00a
Tristan <tristan@larusic.com>
2021-03-22 18:51:01 -0300

    Created category data class

---
+++ lib/data/category.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+
+class Category {
+  final String categoryName;
+  final int categoryID;
+
+  Category({
+    this.categoryName,
+    this.categoryID
+  });
+}
+
+Category sports = Category(
+  categoryName: 'Sports',
+  categoryID: 9
+);
+
+List<Category> categories = [
+    Category(categoryID: 9, categoryName: "General Knowledge" ),
+    Category(categoryID: 10, categoryName: "Entertainment: Books" ),
+    Category(categoryID: 11, categoryName: "Entertainment: Film" ),
+    Category(categoryID: 12, categoryName: "Entertainment: Music" ),
+    Category(categoryID: 13, categoryName: "Entertainment: Musicals & Theatres" ),
+    Category(categoryID: 14, categoryName: "Entertainment: Television" ),
+    Category(categoryID: 15, categoryName: "Entertainment: Video Games" ),
+    Category(categoryID: 16, categoryName: "Entertainment: Board Games" ),
+    Category(categoryID: 17, categoryName: "Science & Nature" ),
+    Category(categoryID: 18, categoryName: "Science: Computers" ),
+    Category(categoryID: 19, categoryName: "Science: Mathematics" ),
+    Category(categoryID: 20, categoryName: "Mythology" ),
+    Category(categoryID: 21, categoryName: "Sports" ),
+    Category(categoryID: 22, categoryName: "Geography" ),
+    Category(categoryID: 23, categoryName: "History" ),
+    Category(categoryID: 24, categoryName: "Politics" ),
+    Category(categoryID: 25, categoryName: "Art" ),
+    Category(categoryID: 26, categoryName: "Celebrities" ),
+    Category(categoryID: 27, categoryName: "Animals" ),
+    Category(categoryID: 28, categoryName: "Vehicles" ),
+    Category(categoryID: 29, categoryName: "Entertainment: Comics" ),
+    Category(categoryID: 30, categoryName: "Science: Gadgets" ),
+    Category(categoryID: 31, categoryName: "Entertainment: Japanese Anime & Manga" ),
+    Category(categoryID: 32, categoryName: "Entertainment: Cartoon & Animations" ),
+  ];
